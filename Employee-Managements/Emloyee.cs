93a0f63550581e62e56392156a926a9dda38ee39
quynhanhsang <sangtrank64@gmail.com>
2022-09-01 11:41:47 +0700

    first commit

---
+++ Employee-Managements/Emloyee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Managements
+{
+    internal class Class1
+    {
+    }
+}
